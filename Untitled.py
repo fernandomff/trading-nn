e412688042d0e0dc78df2beeece3beb01e08dcb7
Fernando Fortunato <fernandofortunato@gmail.com>
2020-11-23 12:53:13 +0000

    commit initial

---
+++ Untitled.py
@@ -0,0 +1,18 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.7.1
+#   kernelspec:
+#     display_name: Python [conda env:tf2]
+#     language: python
+#     name: conda-env-tf2-py
+# ---
+
+# %%
+a= 1 + 1
+
+# %%
